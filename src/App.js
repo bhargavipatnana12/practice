813800d6485cc38cf02b68601dd613dd9a2dff1b
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-20 19:50:59 +0530

    Functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-//Creating and nesting components
 import React from "react";
-function Mybutton() {
-  //Mybutton is a  React component..always starts with capital letter.
-  return (
-    <button onClick={() => alert("hello sruthi..how r you..?")}>
-      I'm a buttom
-    </button>
-  );
+function Oldversion() {
+  console.log("hello");
 }
+
+const Newversion = () => {
+  console.log("hii");
+};
+
+const Newvreturn = (n1, n2) => {
+  return n1 + n2;
+};
+console.log(Newvreturn(2, 4));
+
 const App = () => {
   return (
     <div>
       <h1>HELLO WORLD</h1>
-      <Mybutton />
+      <Oldversion />
+      <Newversion />
+      <Newvreturn />
     </div>
   );
 };
