dbacaf02afd8059a149420a12176e13c7f6649e0
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-23 20:29:39 +0530

    onChange EventHandler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  useEffect(() => console.log("clicked"), []);
+  const [user, setUser] = useState("");
+  const handler = (e) => {
+    setUser(e.target.value);
+  };
   return (
     <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <input
+        type="text"
+        value={user}
+        placeholder="enter text"
+        onChange={handler}
+      />
+      <br />
+      {user}
     </div>
   );
 };
