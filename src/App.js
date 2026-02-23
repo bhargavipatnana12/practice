349a47b4c762263d8f4b60658a4719fae46974fe
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-23 21:23:32 +0530

    onSubmitHandler

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [user, setUser] = useState("");
-  const handler = (e) => {
-    setUser(e.target.value);
+  const [data, setData] = useState({ username: "", password: "" });
+  const { username, password } = data;
+
+  const handler = (e) =>
+    setData({ ...data, [e.target.name]: [e.target.value] });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
   };
   return (
     <div>
-      <input
-        type="text"
-        value={user}
-        placeholder="enter text"
-        onChange={handler}
-      />
-      <br />
-      {user}
+      <form onSubmit={submitHandler}>
+        <input
+          type="username"
+          name="username"
+          value={username}
+          onChange={handler}
+          placeholder="enter username"
+        />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handler}
+          placeholder="enter password"
+        />
+        <input type="submit" name="submit" />
+      </form>
     </div>
   );
 };
