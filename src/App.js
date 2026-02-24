0b0812775be472977d57acdeee89cc4cd8cb5371
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-24 13:32:15 +0530

    map function in reactjs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React from "react";
-import { useState } from "react";
 
 const App = () => {
-  const [data, setData] = useState({ username: "", password: "" });
-  const { username, password } = data;
+  const arr = ["react", "js", "viewjs"];
 
-  const handler = (e) =>
-    setData({ ...data, [e.target.name]: [e.target.value] });
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(data);
-  };
+  const array = [{ id: 1 }, { name: "sruthi" }, { place: "vsd" }];
   return (
     <div>
-      <form onSubmit={submitHandler}>
-        <input
-          type="username"
-          name="username"
-          value={username}
-          onChange={handler}
-          placeholder="enter username"
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handler}
-          placeholder="enter password"
-        />
-        <input type="submit" name="submit" />
-      </form>
+      {arr.map((value, index) => (
+        <li>{value}</li>
+      ))}
+      {array.map((value) => (
+        <li key={value.id}>
+          {value.name}
+          {value.place}
+        </li>
+      ))}
     </div>
   );
 };
