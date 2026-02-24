f439de4f8401bec679095051b41fc1928d7206cf
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-24 19:28:13 +0530

    fetch() in reactjs

--- src/App.js
+++ src/App.js
@@ -1,63 +1,17 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [data, setData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmpassword: "",
-  });
-  const { username, email, password, confirmpassword } = data;
-  const changeHandler = (e) => {
-    setData({ ...data, [e.target.name]: [e.target.value] });
-  };
-  const submitHandler = (e) => e.preventDefault();
-
-  if (password === confirmpassword) {
-    console.log(data);
-  } else {
-    console.log("passwords r not matching");
-  }
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
   return (
     <div>
-      <center>
-        <form onSubmit={submitHandler}>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            placeholder="enter username"
-            onChange={changeHandler}
-          />
-          <br />
-          <input
-            type="email"
-            name="email"
-            value={email}
-            placeholder="enter email"
-            onChange={changeHandler}
-          />
-          <br />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="enter password"
-            onChange={changeHandler}
-          />{" "}
-          <br />
-          <input
-            type="password"
-            name="confirmpassword"
-            value={confirmpassword}
-            placeholder="enter password"
-            onChange={changeHandler}
-          />
-          <br />
-          <input type="submit" name="submit" />
-        </form>
-      </center>
+      {data.map((item) => (
+        <li key={item.id}>{item.username}</li>
+      ))}
     </div>
   );
 };
