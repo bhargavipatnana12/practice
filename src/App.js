f4d8e993316ba00cd9ffaee1e841ea4edfbdaf5c
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-24 19:07:38 +0530

    signup form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const filtered = arr.filter((item) => item > 5);
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  });
+  const { username, email, password, confirmpassword } = data;
+  const changeHandler = (e) => {
+    setData({ ...data, [e.target.name]: [e.target.value] });
+  };
+  const submitHandler = (e) => e.preventDefault();
+
+  if (password === confirmpassword) {
+    console.log(data);
+  } else {
+    console.log("passwords r not matching");
+  }
   return (
     <div>
-      {filtered.map((item) => (
-        <li>{item}</li>
-      ))}
+      <center>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            placeholder="enter username"
+            onChange={changeHandler}
+          />
+          <br />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="enter email"
+            onChange={changeHandler}
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="enter password"
+            onChange={changeHandler}
+          />{" "}
+          <br />
+          <input
+            type="password"
+            name="confirmpassword"
+            value={confirmpassword}
+            placeholder="enter password"
+            onChange={changeHandler}
+          />
+          <br />
+          <input type="submit" name="submit" />
+        </form>
+      </center>
     </div>
   );
 };
