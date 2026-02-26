56ae4eacaff104c89acde8cc4f3620c2a0df11ea
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-26 10:15:46 +0530

    state in class level component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  const [input, setInput] = useState("");
-  const [result, setResult] = useState(0);
-  const handler = (e) => setInput(e.target.value);
-  return (
-    <div>
-      <input name="input" type="text" value={input} onChange={handler} />
-      <button
-        onClick={() => {
-          setResult(eval(input));
-        }}
-      >
-        Result
-      </button>
-      <h4>Result is : {result}</h4>
-      <button onClick={() => setInput(input + "1")}>1</button>
-      <button onClick={() => setInput(input + "2")}>2</button>
-      <button onClick={() => setInput(input + "3")}>3</button>
-      <button onClick={() => setInput(input + "4")}>4</button>
-      <button onClick={() => setInput(input + "5")}>5</button>
-      <br />
-      <button onClick={() => setInput(input + "6")}>6</button>
-      <button onClick={() => setInput(input + "7")}>7</button>
-      <button onClick={() => setInput(input + "8")}>8</button>
-      <button onClick={() => setInput(input + "9")}>9</button>
-      <button onClick={() => setInput(input + "0")}>0</button>
-      <br />
-      <button onClick={() => setInput(input + "+")}>+</button>
-      <button onClick={() => setInput(input + "-")}>-</button>
-      <button onClick={() => setInput(input + "*")}>*</button>
-      <button onClick={() => setInput(input + "/")}>/</button>
-      <button onClick={() => setInput("")}>clr</button>
-    </div>
-  );
-};
-
-export default App;
+export default class App extends Component {
+  state = { name: "sruthi" };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+}
