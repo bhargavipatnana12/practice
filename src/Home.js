db13bd486cafa24cc904536474c5a797fcc6d1b7
bhargavipatnana12 <bhargavipatnana12@gmail.com>
2026-02-27 11:49:55 +0530

    Navigate

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,14 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 const Home = () => {
+  const [auth, setAuth] = useState(false);
+  if (auth) {
+    return <Navigate to="/dashboard" />;
+  }
   return (
     <div>
       <h1>WELCOME TO HOME PAGE</h1>
+      <button onClick={() => setAuth(true)}>Login</button>
     </div>
   );
 };
